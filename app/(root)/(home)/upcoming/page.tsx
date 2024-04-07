cf30965085b503fd33fa5b09f1796ca5a1b7e329
MuhamadFaheem <faheemf428@gmail.com>
2024-04-07 13:08:49 +0700

    feat: first commit of my project (haven't implement the logic yet, just push the UI)

---
+++ app/(root)/(home)/upcoming/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Upcoming = () => {
+  return (
+    <section className='flex size-full flex-col gap-10 text-white'>
+    <h1 className='text-4xl font-bold'>
+      Upcoming
+    </h1>
+
+    
+  </section>
+  )
+}
+
+export default Upcoming
