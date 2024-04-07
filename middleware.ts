271569feeafbf0ea9f1aa275ed993ad9e6136f86
MuhamadFaheem <faheemf428@gmail.com>
2024-04-07 14:23:21 +0700

    feat: add clerk but not finished yet

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+//import { authMiddleware  } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+ 
+// See https://clerk.com/docs/references/nextjs/auth-middleware
+// for more information about configuring your Middleware
+const protectedRoutes = createRouteMatcher([
+  "/",
+  "/upcoming",
+  "/previous",
+  "/recordings",
+  "/personal-room",
+  "/meeting(.*)",
+])
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoutes(req)) auth().protect()
+});
+ 
+export const config = {
+  matcher: [
+    // Exclude files with a "." followed by an extension, which are typically static files.
+    // Exclude files in the _next directory, which are Next.js internals.
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    // Re-include any files in the api or trpc folders that might have an extension
+    "/(api|trpc)(.*)"
+  ]
+};
